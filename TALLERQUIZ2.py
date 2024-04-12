37033276d5cf432581522098cf2080dcee7daa91
mariacamilato <mariac.tobon@udea.edu.co>
2024-04-12 11:13:39 -0500

    punto 6 archivo csv y mat

--- TALLERQUIZ2.py
+++ TALLERQUIZ2.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.io as sp
 
-#PUNTO1:
+#PUNTO 1:
 MATRIZ1=np.random.rand(10,10,10,1200)
 print(MATRIZ1) 
 print("\n")
 
-#PUNTO2:
+#PUNTO 2:
 MATRIZ2=MATRIZ1[:,:,:,0].copy()
 print(MATRIZ2)
 print("\n")
 
-#PUNTO3:
+#PUNTO 3:
 print(f"La forma de la matriz es >>> {MATRIZ2.shape}")
 print(f"El total de datos de la matriz es >>> {MATRIZ2.size}")
 print(f"Las dimensiones de la matriz son >>> {MATRIZ2.ndim}")
@@ -22,14 +22,36 @@ print(f"itemsize de la matriz >>> {MATRIZ2.itemsize}")
 print(f"nbytes de la matriz >>> {MATRIZ2.nbytes}")
 print("\n")
 
-#PUNTO4:
+#PUNTO 4:
 MATRIZ3=MATRIZ2.reshape((10,100))
 print(MATRIZ3)
 print("\n")
 
-#PUNTO5:
+#PUNTO 5:
 def dataframe(MATRIZ3):
     M=pd.DataFrame(MATRIZ3)
     return M 
-dataframe(MATRIZ3)
-print("\n")
+#Llamado de la función
+a=dataframe(MATRIZ3)
+print(a)
+
+#PUNTO 6:
+def archiMatCsv():
+    while True:
+        usuario=input("Ingrese en mayúscula la letra M , si desea cargar un archivo mat \nIngrese la letra C en mayúscula, si desea cargar un archivo csv \n>>>>>  ")
+        if usuario =="M":
+            x=input("Ingrese la ruta del archivo MAT >>> ")
+            mat=sp.loadmat(x)
+            return mat 
+        elif usuario == "C":
+            y=input("Ingrese la ruta del archivo CSV >>> ")
+            csv=pd.read_csv(y)
+            return csv
+        else:
+            print("OPCIÓN INCORRECTA, INGRESE M O C EN MAYUSCULA")
+            continue 
+#Llamado de la función      
+b=archiMatCsv()
+print(b)
+
+
