c5a208b75d79b5f3808523fad80f45f3ffc59835
mariacamilato <mariac.tobon@udea.edu.co>
2024-04-12 10:48:28 -0500

    punto 4 y 5 -pandas

--- TALLERQUIZ2.py
+++ TALLERQUIZ2.py
@@ -6,10 +6,12 @@ import scipy.io as sp
 #PUNTO1:
 MATRIZ1=np.random.rand(10,10,10,1200)
 print(MATRIZ1) 
+print("\n")
 
 #PUNTO2:
 MATRIZ2=MATRIZ1[:,:,:,0].copy()
 print(MATRIZ2)
+print("\n")
 
 #PUNTO3:
 print(f"La forma de la matriz es >>> {MATRIZ2.shape}")
@@ -18,7 +20,16 @@ print(f"Las dimensiones de la matriz son >>> {MATRIZ2.ndim}")
 print(f"Tipo de datos de la matriz >>> {MATRIZ2.dtype}")
 print(f"itemsize de la matriz >>> {MATRIZ2.itemsize}")
 print(f"nbytes de la matriz >>> {MATRIZ2.nbytes}")
+print("\n")
 
 #PUNTO4:
-MATRIZ3=MATRIZ2.reshape
+MATRIZ3=MATRIZ2.reshape((10,100))
 print(MATRIZ3)
+print("\n")
+
+#PUNTO5:
+def dataframe(MATRIZ3):
+    M=pd.DataFrame(MATRIZ3)
+    return M 
+dataframe(MATRIZ3)
+print("\n")
